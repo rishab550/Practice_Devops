114fbf1cde73b5c5b97ea1c721f33ed578770c6b
Rishabh Garg <rishabhgarg1sep@gmail.com>
2024-06-26 14:11:07 +0530

    React_Project_todoApp

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -11,16 +11,29 @@ const handleParams = (a: string) => {
   return a;
 };
 
+interface Person {
+  name: string;
+  age: number;
+}
 
 const App = () => {
   const [val, setVal] = useState<string>("");
-  const [displaySum, setSum] = useState<number | undefined>()
+  const [displaySum, setSum] = useState<number | undefined>();
+  const [user, setUser] = useState<Person>({
+    name: "",
+    age: 0,
+  });
+  const [displayInfo, setDisplayInfo] = useState<Person | undefined>();
+
+  const handleDisplayUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setDisplayInfo(user);
+  };
 
   const handleBtn = (a: number, b: number) => {
     let sum = a + b;
     return setSum(sum);
   };
-  
 
   return (
     <>
@@ -28,9 +41,28 @@ const App = () => {
       <Box func={default_function} func2={handleParams} func3={handleBtn} />
       <div></div>
       <Box2 label={"form label"} value={val} setVal={setVal} />
-      <div>
-        {displaySum}
-      </div>
+      <div>{displaySum}</div>
+      <form onSubmit={handleDisplayUser}>
+        <input
+          type="text"
+          placeholder="Please type your name"
+          value={user?.name}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <input
+          type="number"
+          placeholder="Please type your age"
+          value={user?.age}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev, age: Number(e.target.value) }))
+          }
+        />
+        <button type="submit">Register</button>
+      </form>
+      <div>{displayInfo?.age}</div>
+      <div>{displayInfo?.name}</div>
     </>
   );
 };
